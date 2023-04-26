a89a9d7a86dcab797ec35928203283bc5fafc188
somerandomguy64 <faizan163123@gmail.com>
2023-04-26 13:25:47 +0100

    added a line

--- src/boundary.js
+++ src/boundary.js
@@ -1,5 +1,23 @@
+import Phaser from "phaser";
+
 class Boundary {
-    constructor (
-        
-    )
+  constructor(scene, x1, y1, x2, y2) {
+    this.scene = scene;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  create() {
+    const line = new Phaser.Geom.Line(this.x1, this.y1, this.x2, this.y2);
+
+    const graphics = this.scene.add.graphics({
+      lineStyle: { width: 4, color: 0xaa00aa },
+    });
+
+    graphics.strokeLineShape(line);
+  }
 }
+
+export default Boundary;
--- src/game.js
+++ src/game.js
@@ -1,3 +1,24 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
+import Boundary from "./boundary";
 
-new Phaser.Game()
+class MainScene {
+  preload() {}
+
+  create() {
+    let wall = new Boundary(this, 10, 10, 100, 100);
+    wall.create();
+  }
+
+  update() {}
+}
+
+let config = {
+  width: 700,
+  height: 400,
+  backgroundColor: "#3498DB",
+  scene: MainScene,
+  physics: { default: "arcade" },
+  parent: "game",
+};
+
+new Phaser.Game(config);
